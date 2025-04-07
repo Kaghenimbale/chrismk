c6fc29b2a9283f7b3e5f03799637bb1741a99d7a
Kagheni Mbale <kaghenimbale@gmail.com>
2025-04-07 08:04:32 +0200

    Add collapse

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
-import { IoIosCall } from "react-icons/io";
+import { IoIosCall, IoMdCloseCircle } from "react-icons/io";
 import { FaAngleRight } from "react-icons/fa";
 
 const page = () => {
+  const [isOpen, setOpen] = useState(false);
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
   return (
     <div className="mt-[5rem] w-[70vw] mx-auto py-10 ">
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
@@ -53,11 +58,26 @@ const page = () => {
         </div>
 
         <div className="flex items-center justify-center">
-          <button className="bg-red-700 text-white p-3 w-fit rounded-md transition-all delay-100 hover:bg-white hover:text-red-700 hover:border hover:border-red-700 flex gap-2 items-center">
+          <button
+            className="bg-red-700 text-white p-3 w-fit rounded-md transition-all delay-100 hover:bg-white hover:text-red-700 hover:border hover:border-red-700 flex gap-2 items-center"
+            onClick={handleClick}
+          >
             Book A Service <FaAngleRight />
           </button>
         </div>
       </div>
+      {isOpen ? (
+        <div className="absolute w-[100%] h-[100%] backdrop-blur-md top-0 left-0 z-10">
+          <button
+            className="flex items-center justify-center absolute top-10 right-10"
+            onClick={handleClick}
+          >
+            <IoMdCloseCircle className="font-extrabold text-5xl text-red-700" />
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
